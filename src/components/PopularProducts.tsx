c720073c581b287ad81760bf4dce5f6a975a06f4
Danila <boltach13022001@gmail.com>
2023-06-07 06:06:25 +0400

    add animates

--- src/components/PopularProducts.tsx
+++ src/components/PopularProducts.tsx
@@ -1,12 +1,24 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import SaleUP from "@/components/SaleUP";
 import AboutQuality from "@/components/AboutQuality";
 import Comments from "@/components/Comments";
 import Sponsers from "@/components/Sponsers";
 import Link from "next/link";
+import apiElectronics from "../api/apiElectronics.json";
+import { motion, AnimatePresence } from "framer-motion";
+
+interface IApi {
+  id: number;
+  url: string;
+  title: string;
+  price: number;
+  star: number;
+  color: string;
+  category: string;
+}
 
 type TNamesLi = {
   Cameras: string;
@@ -23,9 +35,35 @@ const NamesLi: TNamesLi = {
 };
 
 export default function PopularProducts() {
+  const [images, setImages] = useState<string[]>([]);
+
   const handleClick = (name: string) => {
     console.log(name);
   };
+
+  const variants = {
+    initial: { opacity: 0, scale: 0.5 },
+
+    visible: (index: number) => ({
+      opacity: 1,
+      scale: 1,
+      transition: { duration: 1 + (index + 1) * 0.3 },
+    }),
+  };
+
+  useEffect(() => {
+    const tempApi = apiElectronics;
+
+    let tempArrUrl: string[] = [];
+
+    for (let i = 0; i < 4; i++) {
+      tempApi.map((value) => {
+        tempArrUrl.push(value.url);
+      });
+    }
+
+    setImages(tempArrUrl);
+  }, []);
 
   return (
     <section className="max-w-[1300px] m-auto">
@@ -64,224 +102,48 @@ export default function PopularProducts() {
         </div>
       </div>
 
-      <section className="grid grid-cols-4 gap-6">
-        <div className="flex flex-col border rounded-xl p-4">
-          <div className="flex justify-center relative">
-            <img
-              className="absolute right-0"
-              src="/images/favourites.png"
-              alt="favourites"
-            />
-            <Image
-              src="/images/popular-product-1.png"
-              width={180}
-              height={180}
-              alt="popular-item"
-            />
-          </div>
+      <motion.section className="grid grid-cols-4 gap-6">
+        {images.map((value, index) => {
+          return (
+            <motion.div
+              custom={index}
+              initial="initial"
+              variants={variants}
+              viewport={{ once: true }}
+              whileInView="visible"
+              key={index}
+              className="flex flex-col border rounded-xl p-4"
+            >
+              <div className="flex justify-center relative">
+                <img
+                  className="absolute right-0 cursor-pointer"
+                  src="/images/favourites.png"
+                  alt="favourites"
+                />
+                <Image
+                  className="object-contain w-[235px] h-[200px]"
+                  src={value}
+                  width={180}
+                  height={180}
+                  alt="popular-item"
+                />
+              </div>
 
-          <div className="flex flex-col gap-4">
-            <p className="text-[#1B5A7D] font-semibold">Photik</p>
-            <p className="font-bold">$11,70</p>
-            <div className="flex gap-2">
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col border rounded-xl p-4">
-          <div className="flex justify-center relative">
-            <img
-              className="absolute right-0"
-              src="/images/favourites.png"
-              alt="favourites"
-            />
-            <Image
-              src="/images/popular-product-1.png"
-              width={180}
-              height={180}
-              alt="popular-item"
-            />
-          </div>
-
-          <div className="flex flex-col gap-4">
-            <p className="text-[#1B5A7D] font-semibold">Photik</p>
-            <p className="font-bold">$11,70</p>
-            <div className="flex gap-2">
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col border rounded-xl p-4">
-          <div className="flex justify-center relative">
-            <img
-              className="absolute right-0"
-              src="/images/favourites.png"
-              alt="favourites"
-            />
-            <Image
-              src="/images/popular-product-1.png"
-              width={180}
-              height={180}
-              alt="popular-item"
-            />
-          </div>
-
-          <div className="flex flex-col gap-4">
-            <p className="text-[#1B5A7D] font-semibold">Photik</p>
-            <p className="font-bold">$11,70</p>
-            <div className="flex gap-2">
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col border rounded-xl p-4">
-          <div className="flex justify-center relative">
-            <img
-              className="absolute right-0"
-              src="/images/favourites.png"
-              alt="favourites"
-            />
-            <Image
-              src="/images/popular-product-1.png"
-              width={180}
-              height={180}
-              alt="popular-item"
-            />
-          </div>
-
-          <div className="flex flex-col gap-4">
-            <p className="text-[#1B5A7D] font-semibold">Photik</p>
-            <p className="font-bold">$11,70</p>
-            <div className="flex gap-2">
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col border rounded-xl p-4">
-          <div className="flex justify-center relative">
-            <img
-              className="absolute right-0"
-              src="/images/favourites.png"
-              alt="favourites"
-            />
-            <Image
-              src="/images/popular-product-1.png"
-              width={180}
-              height={180}
-              alt="popular-item"
-            />
-          </div>
-
-          <div className="flex flex-col gap-4">
-            <p className="text-[#1B5A7D] font-semibold">Photik</p>
-            <p className="font-bold">$11,70</p>
-            <div className="flex gap-2">
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col border rounded-xl p-4">
-          <div className="flex justify-center relative">
-            <img
-              className="absolute right-0"
-              src="/images/favourites.png"
-              alt="favourites"
-            />
-            <Image
-              src="/images/popular-product-1.png"
-              width={180}
-              height={180}
-              alt="popular-item"
-            />
-          </div>
-
-          <div className="flex flex-col gap-4">
-            <p className="text-[#1B5A7D] font-semibold">Photik</p>
-            <p className="font-bold">$11,70</p>
-            <div className="flex gap-2">
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col border rounded-xl p-4">
-          <div className="flex justify-center relative">
-            <img
-              className="absolute right-0"
-              src="/images/favourites.png"
-              alt="favourites"
-            />
-            <Image
-              src="/images/popular-product-1.png"
-              width={180}
-              height={180}
-              alt="popular-item"
-            />
-          </div>
-
-          <div className="flex flex-col gap-4">
-            <p className="text-[#1B5A7D] font-semibold">Photik</p>
-            <p className="font-bold">$11,70</p>
-            <div className="flex gap-2">
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col border rounded-xl p-4">
-          <div className="flex justify-center relative">
-            <img
-              className="absolute right-0"
-              src="/images/favourites.png"
-              alt="favourites"
-            />
-            <Image
-              src="/images/popular-product-1.png"
-              width={180}
-              height={180}
-              alt="popular-item"
-            />
-          </div>
-
-          <div className="flex flex-col gap-4">
-            <p className="text-[#1B5A7D] font-semibold">Photik</p>
-            <p className="font-bold">$11,70</p>
-            <div className="flex gap-2">
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-              <img src="/images/star.png" alt="star" />
-            </div>
-          </div>
-        </div>
-      </section>
+              <div className="flex flex-col gap-4">
+                <p className="text-[#1B5A7D] font-semibold">Photik</p>
+                <p className="font-bold">$11,70</p>
+                <div className="flex gap-2">
+                  <img src="/images/star.png" alt="star" />
+                  <img src="/images/star.png" alt="star" />
+                  <img src="/images/star.png" alt="star" />
+                  <img src="/images/star.png" alt="star" />
+                  <img src="/images/star.png" alt="star" />
+                </div>
+              </div>
+            </motion.div>
+          );
+        })}
+      </motion.section>
       <div className="flex gap-2 justify-center my-[50px]">
         <span className="w-[15px] h-[15px]  border bg-[#ff8d41] rounded-md block"></span>
         <span className="w-[15px] h-[15px]  border rounded-md block"></span>
--- src/components/SaleUP.tsx
+++ src/components/SaleUP.tsx
@@ -1,15 +1,37 @@
 import React from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function SaleUP() {
+  const variants = {
+    initial: { scale: 1.5 },
+
+    visible: (index: number) => ({
+      scale: 1,
+      transition: { duration: 2 },
+    }),
+  };
+
   return (
-    <section className="max-w-[1300px] m-auto flex bg-[url('/images/bg-sale.png')] rounded-3xl justify-end bg-no-repeat my-[50px]">
+    <motion.section
+      initial="initial"
+      whileInView="visible"
+      variants={variants}
+      viewport={{ once: true }}
+      className="max-w-[1300px] m-auto flex bg-[url('/images/bg-sale.png')] rounded-3xl justify-end bg-no-repeat my-[50px]"
+    >
       <div className="flex flex-col items-center pt-[90px] pb-[60px] pr-[150px] gap-4">
-        <button className="bg-[#d99613] py-[10px] px-[25px] text-white rounded-2xl">New laptop</button>
-        <h1 className="text-[50px] text-[#2E8FC5] font-bold">Sale up to 50% off</h1>
+        <button className="bg-[#d99613] py-[10px] px-[25px] text-white rounded-2xl">
+          New laptop
+        </button>
+        <h1 className="text-[50px] text-[#2E8FC5] font-bold">
+          Sale up to 50% off
+        </h1>
         <p className="text-white">12 inch hd display</p>
-        <button className="bg-[#d99613] py-[10px] px-[25px] text-white rounded-2xl">Shop now</button>
+        <button className="bg-[#d99613] py-[10px] px-[25px] text-white rounded-2xl">
+          Shop now
+        </button>
       </div>
-    </section>
+    </motion.section>
   );
 }
